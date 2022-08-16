a0b84449dc8f2f1cdec481822c49f7595f2db9d8
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2022-08-16 20:46:59 +0300

    Добавлен UI на MainViewController

--- ARHitBalls/Scenes/Main/MainPresenter.swift
+++ ARHitBalls/Scenes/Main/MainPresenter.swift
@@ -5,9 +5,14 @@
 //  Created by Swift Learning on 15.08.2022.
 //
 
+import UIKit
+
 // MARK: -  MainPresenterProtocol
 protocol  MainPresenterProtocol: AnyObject {
-    
+    func settingsButtonPressed()
+    func startQuickGameButtonPressed()
+    func logOutButtonPressed()
+    func missionStartGameButtonPressed()
 }
 
 // MARK: -  MainPresenter
@@ -24,5 +29,21 @@ final class  MainPresenter {
 
 //MARK: -  MainPresenterExtension
 extension  MainPresenter: MainPresenterProtocol {
+    func settingsButtonPressed() {
+        let rootViewController = sceneBuildManager.buildSettingsScreen()
+        UIApplication.shared.windows.first?.rootViewController = rootViewController
+    }
     
+    func startQuickGameButtonPressed() {
+        print(#function)
+    }
+    
+    func logOutButtonPressed() {
+        let rootViewController = sceneBuildManager.buildMenuScreen()
+        UIApplication.shared.windows.first?.rootViewController = rootViewController
+    }
+    
+    func missionStartGameButtonPressed() {
+        print(#function)
+    }
 }
