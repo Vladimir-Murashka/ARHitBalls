983f6dc9db1acbdcccb1aa657d47187379d9636e
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2022-11-08 16:05:31 +0300

    Добавлена анимация загрузки на SplashViewController

--- ARHitBalls/Scenes/Splash/SplashViewController.swift
+++ ARHitBalls/Scenes/Splash/SplashViewController.swift
@@ -24,12 +24,21 @@ final class SplashViewController: UIViewController {
         return imageView
     }()
     
+    private var shapeLayer = CAShapeLayer()
+    private var staticShapeLayer = CAShapeLayer()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViewController()
         presenter?.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        staticDownloadLine()
+        animation()
     }
 }
 
@@ -43,6 +52,68 @@ private extension SplashViewController {
     func setupViewController() {
         addSubViews()
         setupConstraints()
+    }
+    
+    func animation() {
+        
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let width = Int(screenSize.size.width)
+        let heigth = Int(screenSize.size.height)
+        
+        let startXPosition = width / 4
+        let finishXPosition = width / 4 * 3
+        
+        let yPosition = heigth - 200
+        
+        self.shapeLayer.removeFromSuperlayer()
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: startXPosition, y: yPosition))
+        path.addLine(to: CGPoint(x: finishXPosition, y: yPosition))
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = #colorLiteral(red: 0, green: 0.6087715843, blue: 1, alpha: 1).cgColor
+        shapeLayer.lineWidth = 6
+        shapeLayer.path = path.cgPath
+        
+        view.layer.addSublayer(shapeLayer)
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0
+        animation.duration = 5
+        shapeLayer.add(animation, forKey: "MyAnimation")
+        
+        self.shapeLayer = shapeLayer
+        
+    }
+    
+    func staticDownloadLine() {
+        
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let width = Int(screenSize.size.width)
+        let heigth = Int(screenSize.size.height)
+        
+        let startXPosition = width / 4
+        let finishXPosition = width / 4 * 3
+        
+        let yPosition = heigth - 200
+        
+        self.staticShapeLayer.removeFromSuperlayer()
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: startXPosition, y: yPosition))
+        path.addLine(to: CGPoint(x: finishXPosition, y: yPosition))
+        
+        let staticShapeLayer = CAShapeLayer()
+        staticShapeLayer.strokeColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        staticShapeLayer.lineWidth = 6
+        staticShapeLayer.path = path.cgPath
+        
+        view.layer.addSublayer(staticShapeLayer)
+
+        self.staticShapeLayer = staticShapeLayer
+        
     }
     
     func addSubViews() {
